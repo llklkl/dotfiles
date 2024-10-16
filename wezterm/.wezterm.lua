88ffa95462d20136e0d3b964598325cc413f1a6a
llklkl <fengl6638@gmail.com>
2024-10-16 14:49:34 +0800

    更新wezterm配置

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,6 +2,28 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 local is_windows = wezterm.target_triple == "x86_64-pc-windows-msvc"
 
+function safe_require(name)
+	local success, module = pcall(require, name)
+	if success then
+		return module
+	else
+		return nil
+	end
+end
+
+-- 本地特定配置
+local loc = {
+	-- 初始列数
+	initial_cols = 120,
+	-- 初始行数
+	initial_rows = 35,
+}
+local lc = safe_require("local")
+if lc then
+	if lc.initial_rows then loc.initial_rows = lc.initial_rows end
+	if lc.initial_cols then loc.initial_cols = lc.initial_cols end
+end
+
 -- 启动菜单
 local launch = {}
 
@@ -31,6 +53,9 @@ local key_tables = {
 	resize_pane = {
 		{ key = "LeftArrow", action = act.AdjustPaneSize({ "Left", 1 }) },
 		{ key = "RightArrow", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "DownArrow", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "UpArrow", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "q", action = "PopKeyTable" },
 		{ key = "Escape", action = "PopKeyTable" },
 	},
 }
@@ -54,8 +79,8 @@ return {
 	-- 窗口设置
 	window_decorations = "INTEGRATED_BUTTONS|RESIZE",
 	window_close_confirmation = "AlwaysPrompt",
-	initial_cols = 120,
-	initial_rows = 35,
+	initial_cols = loc.initial_cols,
+	initial_rows = loc.initial_rows,
 
 	-- 按键绑定
 	disable_default_key_bindings = true,
